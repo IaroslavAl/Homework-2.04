57d0120ac3cc5b22eb262d540a98b840060da16c
Iaroslav <Iaroslav.aleksandrovich.beldin@gmail.com>
2023-03-24 11:12:02 +0400

    added interaction between sliders and color view

--- Homework 2.04/ViewController.swift
+++ Homework 2.04/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var redSliderIndicator: UILabel!
+    @IBOutlet weak var greenSliderIndicator: UILabel!
+    @IBOutlet weak var blueSliderIndicator: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redSlider.value = 0.5
+        greenSlider.value = 0.5
+        blueSlider.value = 0.5
+        
+        colorView.layer.cornerRadius = 20
     }
-
-
+    
+    @IBAction func sliderChanged() {
+        let red = redSlider.value
+        let green = greenSlider.value
+        let blue = blueSlider.value
+        
+        redSliderIndicator.text = String(format: "%.2f", redSlider.value)
+        greenSliderIndicator.text = String(format: "%.2f", greenSlider.value)
+        blueSliderIndicator.text = String(format: "%.2f", blueSlider.value)
+        
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(red),
+            green: CGFloat(green),
+            blue: CGFloat(blue),
+            alpha: 1
+        )
+    }
+    
 }
 
